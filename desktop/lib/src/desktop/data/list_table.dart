7c38a2993a4ca299926cbfeff6cb428d19aeb786
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-05-15 15:21:29 +0530

    fix: missing import

--- desktop/lib/src/desktop/data/list_table.dart
+++ desktop/lib/src/desktop/data/list_table.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
